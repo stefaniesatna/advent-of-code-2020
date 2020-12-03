1bd7e966293dd8d5f5358b425947a87ba7a5aee3
Stefanie Satna <satnastefani@gmail.com>
2020-12-03 10:14:59 +0000

    Created a movement() and an MVP of countNextTree()

--- 3/src/index.js
+++ 3/src/index.js
@@ -4,6 +4,12 @@ const path = require("path")
 const inputPath = path.resolve(__dirname, "input-day3.txt")
 const bigArr = fs.readFileSync(inputPath, "utf-8").split("\n");
 
+let row = 0;
+let column = 0;
+
+let tree = 0;
+let treeFree = 0;
+
 // remove the last (empty) array from the end of bigArr
 bigArr.pop();
 
@@ -13,5 +19,25 @@ bigArr.forEach(element => {
     masterArr.push(element.split(""));
 });
 
+// write a function that moves 3 right and 1 down, returns back the position
 
-console.log(masterArr);
+function movement() {
+    row++;
+    column += 3;
+    return [row, column];
+}
+
+// write a function that counts trees on coordinates from movement()
+
+function countNextTree(moveFunction){
+    let coordinates = moveFunction();
+    return masterArr[coordinates[0]][coordinates[1]] === "#" ? tree++ : treeFree++;
+}
+
+countNextTree(movement);
+countNextTree(movement);
+countNextTree(movement);
+countNextTree(movement);
+countNextTree(movement);
+countNextTree(movement);
+console.log(tree, treeFree);
