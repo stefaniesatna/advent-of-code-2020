c782e26a9d456362ccabec5bbd5434dd5d59ac19
Stefanie Satna <satnastefani@gmail.com>
2020-12-03 13:56:43 +0000

    Got rid of the functions and added a for loop (couldn't make it work with functions when modifying global variable column)

--- 3/src/index.js
+++ 3/src/index.js
@@ -1,43 +1,35 @@
-const fs = require("fs")
-const path = require("path")
+const fs = require("fs");
+const path = require("path");
 
-const inputPath = path.resolve(__dirname, "input-day3.txt")
+const inputPath = path.resolve(__dirname, "input-day3.txt");
 const bigArr = fs.readFileSync(inputPath, "utf-8").split("\n");
 
-let row = 0;
-let column = 0;
-
-let tree = 0;
-let treeFree = 0;
-
-// remove the last (empty) array from the end of bigArr
-bigArr.pop();
-
-// split lines into arrays / make an array of arrays
 let masterArr = [];
 bigArr.forEach(element => {
     masterArr.push(element.split(""));
 });
 
-// write a function that moves 3 right and 1 down, returns back the position
+const arrLength = masterArr[0].length;
 
-function movement() {
-    row++;
-    column += 3;
-    return [row, column];
+let row = 0;
+let column = 0;
+
+const moveDown = 1;
+const moveRight = 3;
+
+let tree = 0;
+let treeFree = 0;
+
+for (let i = 0; i < masterArr.length - 1; i++){
+    row += moveDown;
+    column = (column + moveRight) % arrLength;
+
+    if (masterArr[row][column] === "#"){
+        tree++;
+    }
+    
+    else{
+        treeFree++;
 }
 
-// write a function that counts trees on coordinates from movement()
-
-function countNextTree(moveFunction){
-    let coordinates = moveFunction();
-    return masterArr[coordinates[0]][coordinates[1]] === "#" ? tree++ : treeFree++;
-}
-
-countNextTree(movement);
-countNextTree(movement);
-countNextTree(movement);
-countNextTree(movement);
-countNextTree(movement);
-countNextTree(movement);
-console.log(tree, treeFree);
+console.log("Tree: ", tree, "TreeFree: ", treeFree);
