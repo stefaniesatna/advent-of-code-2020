cf152b633c3dd9017c817918d95b55dcb3add0da
Stefanie Satna <satnastefani@gmail.com>
2020-12-04 14:31:53 +0000

    Added isValid() to check for passport validity and logged the result

--- 4/src/index.js
+++ 4/src/index.js
@@ -3,11 +3,9 @@ const path = require("path");
 
 const inputPath = path.resolve(__dirname, "input-day4.txt");
 const bigArr = fs.readFileSync(inputPath, "utf-8").split("\n\n");
-let newArr = [];
-
-
 
 // Convert chunks of string into an array of strings
+let newArr = [];
 for (let i = 0; i < bigArr.length; i++){
     newArr.push(bigArr[i].split(/\s+/));
 }
@@ -23,7 +21,7 @@ for (let i = 0; i < newArr.length; i++){
     almostFinalArr.push(keyValueArr);
 }
 
-// Convert those arrays with key value pairs into an object 
+// Convert those arrays with key value pairs into objects
 let finalArr = [];
 for (let i = 0; i < almostFinalArr.length; i++){
     const obj = Object.fromEntries(almostFinalArr[i]);
@@ -31,3 +29,21 @@ for (let i = 0; i < almostFinalArr.length; i++){
 }
 
 console.log(finalArr);
+
+// End of Setup ====================================================================
+
+const required = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+
+// function to check validity of passports 
+function isValid(obj){
+    let valid = true;
+    for (let i = 0; i < required.length; i++){
+        if (!obj.hasOwnProperty(required[i])){
+            valid = false; 
+        }
+    }
+    return valid; 
+}
+
+validCount = finalArr.filter(x => isValid(x)).length;
+console.log(validCount);
